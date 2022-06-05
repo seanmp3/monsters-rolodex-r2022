c9e0695b22a7fc508172fa7e637a57d3308c4f0b
sean-mahan <seanmahansean@gmail.com>
2022-06-05 03:46:15 -0500

    reached functionality and optimised what runs in each render

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ class App extends Component{
   constructor(){
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ""
     }
   }
 
@@ -20,10 +21,31 @@ class App extends Component{
       })
   }
 
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase()
+
+    this.setState(() => {
+      return {searchField}
+    })
+}
+
   render(){
+    const {monsters, searchField} = this.state
+    const {onSearchChange} = this
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField)
+    })
+
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => {
+        <input 
+          className="search-box" 
+          type="search" 
+          placeholder="search monsters" 
+          onChange={onSearchChange} 
+        />
+        {filteredMonsters.map((monster) => {
             return (
               <div key={monster.id}>
                 <h1>{monster.name}</h1>
